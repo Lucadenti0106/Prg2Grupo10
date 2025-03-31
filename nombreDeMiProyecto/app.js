2d7edca4dbb1082f6f9910b68594c719a5976972
agustinrolondebary <arolondebary@udesa.edu.ar>
2025-03-31 11:31:23 -0300

    controladores

--- nombreDeMiProyecto/app.js
+++ nombreDeMiProyecto/app.js
@@ -4,14 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-// Import routes
+// Importar todas las rutas
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 
 const app = express();
 
-// View engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,23 +21,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
-// Catch 404 and forward to error handler
+// Error 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
 app.use(function(err, req, res, next) {
-  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- nombreDeMiProyecto/controllers/usercontroller.js
+++ nombreDeMiProyecto/controllers/usercontroller.js
@@ -2,18 +2,10 @@ const userController = {
     login: function(req, res) {
         res.render('login', { title: 'Ingresar' });
     },
-    processLogin: function(req, res) {
-        // Process login logic would go here
-        res.redirect('/');
-    },
     register: function(req, res) {
         res.render('register', { title: 'Registro' });
     },
-    processRegister: function(req, res) {
-        // Process register logic would go here
-        res.redirect('/');
-    },
-    profile: function(req, res) {
+    perfil: function(req, res) {
         res.render('profile', { title: 'Mi perfil' });
     }
 };
