03b6ab1992d31a773a7e7220137dac107269e057
ldenti@udesa.edu.ar <ldenti@udesa.edu.ar>
2025-05-19 14:21:37 -0300

    arrancando parte final

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,7 +6,13 @@ const usersController = {
     },    
     register: (req, res) => {
         res.render("register");
-    },    
+    },
+    
+    processRegister: (req, res) => {
+        const { email, usuario, contrasenia, fecha_nacimiento, dni, foto_perfil } = req.body;
+        let errores = [];},
+        
+       
     profile: (req, res) => {
         res.render("profile", {
             usuario: moduloDatos.usuario,
